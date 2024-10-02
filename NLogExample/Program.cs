f8af76b41ea072cd5fb5f1f52789d33f57cc394b
Arafat Zaman Anik <arafat.zaman.anik@gmail.com>
2024-10-03 03:47:29 +0600

    Add project files.

---
+++ NLogExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using NLog;
+
+class Program
+{
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    static void Main(string[] args)
+    {
+        // Log different levels of messages
+        Logger.Info("Application has started.");
+        Logger.Debug("This is a debug message.");
+        Logger.Warn("Warning! Something might be wrong.");
+        Logger.Error("An error occurred.");
+        Logger.Fatal("Fatal error! Application will shut down.");
+
+        // Simulate application logic
+        try
+        {
+            DivideNumbers(10, 0); // Will cause a divide by zero exception
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex, "An exception occurred.");
+        }
+
+        Logger.Info("Application has finished.");
+    }
+
+    static int DivideNumbers(int number1, int number2)
+    {
+        return number1 / number2; // This will throw an exception if number2 is zero
+    }
+}
